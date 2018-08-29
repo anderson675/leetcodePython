5e77cb5fb7c0a674003773e6f8170df9bb03fc90
anderson675 <cl3844@nyu.edu>
2018-08-29 15:12:59 -0400

    45 / 889

---
+++ 589N-aryTreePreorderTraversal.py
@@ -0,0 +1,18 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        if not root: return []
+        t = [root.val]
+        for i in root.children:
+            t+=self.preorder(i)
+        return t
